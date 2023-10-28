da938c427519c47ece199d617d04667cc6584777
12 <12@DESKTOP-NK7HEP0>
2023-10-28 12:59:12 +0300

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,15 @@
+﻿void summa()
+{
+    Console.WriteLine("Найдем сумму чисел от 100 до 200, которые делятся на 17");
+    int sum = 0;
+    for (int i = 100; i < 200; i++)
+    {
+        if (i % 17 == 0)
+        {
+            sum = sum + i;
+            Console.WriteLine(i);
+        }
+    }
+    Console.WriteLine($"Сумма чисел будет равна {sum}");
+}
+summa();
